57f8a1187ba29250687a23d1c1989b5b7e048982
AMRENDRA RAJ <68627259+AMRENDRAA@users.noreply.github.com>
2024-09-20 01:43:21 +0530

    Update index.js
    updated

--- index.js
+++ index.js
@@ -54,7 +54,6 @@ fontpicker.addEventListener('change',(e)=>{
 ctx.lineWidth=e.target.value;
 
 })
-<<<<<<< HEAD
 clearButton.addEventListener('click',(e)=>{
     ctx.clearRect(0,0,canvas.width,canvas.clientHeight);
 
@@ -67,5 +66,3 @@ saveButton.addEventListener('click',(e)=>{
     link.href=canvas.toDataURL();
     link.click();
 })
-=======
->>>>>>> 9b2a9ce4f5a1db40bec22fd3bef1050c3263c7a3
